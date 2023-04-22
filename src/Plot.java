5758dd1a1b5bef88687eb6fac2175e590eb59992
len10101 <117754846+len10101@users.noreply.github.com>
2023-04-22 11:34:12 -0400

    Completed printPlots method.

--- src/Plot.java
+++ src/Plot.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Plot {
+public class Plot {
+    public String plantname;
+    public int numberofplants;
+    public boolean needswater;
+    public int num;
+
+    public Plot() {
+//        plantname = "sunflower";
+        numberofplants = (int) (Math.random() * 100) + 10;
+        needswater = true;
+
+        num = (int) (Math.random() * 5);
+        if (num == 0) {
+            plantname = "sunflower";
+        }
+        if (num == 1) {
+            plantname = "carrot";
+        }
+        if (num == 2) {
+            plantname = "corn";
+        }
+        if (num == 3) {
+            plantname = "tomato";
+        }
+        if (num == 4) {
+            plantname = "empty";
+        }
+    }
+        public void printPlots() {
+            System.out.println("This plant is a " + plantname + ". There are " + numberofplants + " plants here. That this plant needs water is " + needswater + ".");
+        }
+
 }
--- src/myFarm.java
+++ src/myFarm.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class myFarm {
+public class myFarm {
+    public static void main(String[] args) {
+        myFarm charlie = new myFarm();
+    }
+    public myFarm() {
+        System.out.println("hello from my farm!");
+    }
+
+    //printPlots(); - tried "extends Plot", but it didn't work
 }
