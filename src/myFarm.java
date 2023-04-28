88c143073a506aac48896875a1b79d84c0a2f928
len10101 <117754846+len10101@users.noreply.github.com>
2023-04-28 13:05:06 -0400

    Made totalPlants and printPlantNames methods.

--- src/myFarm.java
+++ src/myFarm.java
@@ -2,13 +2,17 @@ public class myFarm {
     public static void main(String[] args) {
         myFarm charlie = new myFarm();
     }
+
+    Plot [][] grid;
     public myFarm() {
         System.out.println("hello from my farm!");
 
         Plot ryan = new Plot();
         ryan.printPlot();
 
-        Plot [][] grid = new Plot [10][10];
+        //ryan.numberofplants
+
+         grid = new Plot [10][3];
 
         for(int x=0; x<grid.length; x++) {
             for (int y=0; y < grid[x].length; y++) {
@@ -16,11 +20,30 @@ public class myFarm {
                 grid[x][y].printPlot();
             }
         }
-       totalPlants();
-    }
+        totalPlants();
+        printPlantNames();
+        }
 
     public void totalPlants() {
-        for()
-        //how many total plants in the grid?
+        int sumofplants = 0;
+
+        for(int e=0; e<grid.length; e++) {
+            for(int c=0; c<grid[e].length; c++) {
+                //System.out.println(grid[e][c].numberofplants);
+                sumofplants += grid[e][c].numberofplants;
+            }
+        }
+        System.out.println(sumofplants);
+    }
+
+    public void printPlantNames() {
+
+        for (int r = 0; r < grid.length; r++) {
+            for (int c = 0; c < grid[r].length; c++) {
+                System.out.println("row: " + r + " col: " + c + " " + grid[r][c].plantname);
+            }
+        }
     }
 }
+
+
